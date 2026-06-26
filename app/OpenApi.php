825e7203d4c02d0da57526d998d20017380b4317
Renaya Marvela <renayaadoe2@gmail.com>
2026-06-26 11:27:14 +0700

    fix: add security scheme

--- app/OpenApi.php
+++ app/OpenApi.php
@@ -13,6 +13,12 @@ use OpenApi\Attributes as OA;
     url: "http://localhost:8001",
     description: "Local Server"
 )]
+#[OA\SecurityScheme(
+    securityScheme: "ApiKeyAuth",
+    type: "apiKey",
+    in: "header",
+    name: "X-IAE-KEY"
+)]
 class OpenApi
 {
 }
